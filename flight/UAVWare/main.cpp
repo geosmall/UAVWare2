e3bfbc03a35b048900e5ee731f25b82be5b439a4
George Small <geomall92@gmail.com>
2023-01-01 09:32:59 -0500

    Moved sys and board init from main() to UAVWare_init()

--- flight/UAVWare/main.cpp
+++ flight/UAVWare/main.cpp
@@ -14,20 +14,6 @@
 int main( void )
 {
 
-  /* Brings up System using CMSIS functions, initializes periph clock, gpio pins. */
-  UVOS_SYS_Init();
-
-  /* board driver init */
-  if ( UVOS_Board_Init() ) {
-#ifdef UVOS_INCLUDE_DEBUG_CONSOLE
-    UVOS_DEBUG_Panic( "System initialization Error\r\n" );
-#endif // UVOS_INCLUDE_DEBUG_CONSOLE
-  } else {
-#ifdef UVOS_INCLUDE_DEBUG_CONSOLE
-    UVOS_COM_SendString( UVOS_COM_DEBUG, "System initialized\r\n" );
-#endif // UVOS_INCLUDE_DEBUG_CONSOLE
-  }
-
   /* Initialize UAVWare */
   UAVWare_init();
 
